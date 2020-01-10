0b1ff92e5565278822f9f2f93023ebafd4fb2ce3
abhishek-singh-hub <59343539+abhishek-singh-hub@users.noreply.github.com>
2020-01-10 19:35:26 +0530

    Update SpringBootBoImpl.java
    Mongo Integration and app.properties for common resources

--- SpringBootRestApiWithHibernate/src/main/java/SpringBootRestApiWithHibernate/SpringBootRestApiWithHibernate/BO/SpringBootBoImpl.java
+++ SpringBootRestApiWithHibernate/src/main/java/SpringBootRestApiWithHibernate/SpringBootRestApiWithHibernate/BO/SpringBootBoImpl.java
@@ -1,15 +1,24 @@
 package SpringBootRestApiWithHibernate.SpringBootRestApiWithHibernate.BO;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mongodb.client.FindIterable;
+
 import SpringBootRestApiWithHibernate.SpringBootRestApiWithHibernate.DAO.SpringBootDAO;
 import SpringBootRestApiWithHibernate.SpringBootRestApiWithHibernate.DTO.Employee;
 import SpringBootRestApiWithHibernate.SpringBootRestApiWithHibernate.DTO.Student;
+import SpringBootRestApiWithHibernate.SpringBootRestApiWithHibernate.MongoDb.MongoConnectionClass;
 
 @Component
 public class SpringBootBoImpl implements SpringBootBO {
@@ -44,6 +53,21 @@ public class SpringBootBoImpl implements SpringBootBO {
 	public Student getStudentDetailById(Integer studentId) {
 		logger.info("SpringBootBoImpl :-> getStudentDetailById()");
 		return springBootDao.getStudentDetailById(studentId);
+	}
+
+	@Override
+	public List<Map<String, Object>> getEmployeeActivityHistory() {
+		logger.info("SpringBootBoImpl :-> getEmployeeActivityHistory()");
+		List<Map<String, Object>> empActivityHistoryList = new ArrayList<>();
+		MongoConnectionClass mongoConnection = new MongoConnectionClass();
+		FindIterable<Document> collection =  mongoConnection.getMongoDb().getCollection("javaCol").find();
+		Iterator<Document> iterator = collection.iterator();
+		while(iterator.hasNext()) {
+			Document document = iterator.next();
+			Map<String, Object> map = new HashMap<String, Object>(document);
+			empActivityHistoryList.add(map);
+		}
+		return empActivityHistoryList;
 	}
 
 }
