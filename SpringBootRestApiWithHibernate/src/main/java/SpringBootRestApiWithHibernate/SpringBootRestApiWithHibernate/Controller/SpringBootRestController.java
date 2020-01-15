c670f77140d247e651f6176394afec053777fe97
abhishek-singh-hub <59343539+abhishek-singh-hub@users.noreply.github.com>
2020-01-15 21:17:31 +0530

    Update SpringBootRestController.java

--- SpringBootRestApiWithHibernate/src/main/java/SpringBootRestApiWithHibernate/SpringBootRestApiWithHibernate/Controller/SpringBootRestController.java
+++ SpringBootRestApiWithHibernate/src/main/java/SpringBootRestApiWithHibernate/SpringBootRestApiWithHibernate/Controller/SpringBootRestController.java
@@ -104,7 +104,7 @@ public class SpringBootRestController {
 			List<Map<String, Object>> empActivityHistory = springBootBo.getEmployeeActivityHistory();
 			return new ResponseEntity<List>(empActivityHistory,  HttpStatus.OK);
 		} catch (Exception e) {
-			e.printStackTrace();//aks
+			e.printStackTrace();
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.OK);
 		}
 		
